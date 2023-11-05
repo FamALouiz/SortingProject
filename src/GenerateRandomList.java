8baff6189ffcc0981536d164fbff80a721b86a86
FamALouiz <fam@awadlouis.com>
2023-11-05 20:29:08 +0200

    Adding shuffling button and functionality:

---
+++ src/GenerateRandomList.java
@@ -0,0 +1,59 @@
+package src;
+
+import java.util.Random;
+
+public class GenerateRandomList extends Thread {
+
+    int n;
+
+    public GenerateRandomList(int n) {
+        this.n = n;
+    }
+
+    @Override
+    public void run() {
+        try {
+            GenerateRandomList.generateRandomListTest(n);
+        } catch (InterruptedException e) {
+            return;
+        }
+    }
+
+    public static void generateRandomListTest(int n) throws InterruptedException {
+        Random r = new Random();
+        for (int i = 0; i < 1000; i++) {
+            // Generating 2 random numbers and swapping their positions
+            int rr1 = r.nextInt(n);
+            int rr2 = r.nextInt(n);
+            int r1 = Math.min(rr1, rr2);
+            int r2 = Math.max(rr1, rr2);
+
+            // Getting all dimension of random components
+            int r1x, r1y, r1w, r1h, r2x, r2y, r2w, r2h;
+            try {
+                r1x = Main.rects[r1].getX();
+                r1y = Main.rects[r1].getY();
+                r1w = Main.rects[r1].getWidth() - r1x;
+                r1h = Main.rects[r1].getHeight() - r1y;
+
+                r2x = Main.rects[r2].getX();
+                r2y = Main.rects[r2].getY();
+                r2w = Main.rects[r2].getWidth() - r2x;
+                r2h = Main.rects[r2].getHeight() - r2y;
+            } catch (Exception e) {
+                continue;
+            }
+            // Creating new ones in their place
+            Rect temp1 = new Rect(r1x, r2y, r1w, r2h, ((Rect) Main.rects[r2]).getColor());
+            Rect temp2 = new Rect(r2x, r1y, r2w, r1h, ((Rect) Main.rects[r1]).getColor());
+
+            Main.removeRect(r1);
+            Main.removeRect(r2);
+
+            Main.rects[r1] = temp1;
+            Main.rects[r2] = temp2;
+        }
+        Main.removeAllRects();
+        Main.addComp(Main.rects);
+    }
+}
---
+++ src/Rect.java
@@ -0,0 +1,68 @@
+package src;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Component;
+
+public class Rect extends JPanel {
+
+    private int RECT_X;
+    private int RECT_Y;
+    private int RECT_WIDTH;
+    private int RECT_HEIGHT;
+    private Color RECT_COLOR;
+
+    public Rect(int x, int y, int w, int h, Color c) {
+        this.RECT_X = x;
+        this.RECT_Y = y;
+        this.RECT_WIDTH = w;
+        this.RECT_HEIGHT = h;
+        this.RECT_COLOR = c;
+        init();
+    }
+
+    public Rect(Component r, Color c) {
+        this.RECT_COLOR = c;
+        this.RECT_X = r.getX();
+        this.RECT_Y = r.getY();
+        this.RECT_HEIGHT = r.getHeight() - RECT_Y;
+        this.RECT_WIDTH = r.getWidth() - RECT_X;
+        init();
+    }
+
+    // Initializing location and size
+    public void init() {
+        this.setLocation(this.getX(), this.getY());
+        Dimension size = this.getPreferredSize();
+        this.setSize((int) size.width, (int) size.height);
+    }
+
+    // Drawing the rectangle on screen
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(RECT_COLOR);
+        g.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(RECT_WIDTH + RECT_X, RECT_HEIGHT + RECT_Y);
+    }
+
+    @Override
+    public int getX() {
+        return this.RECT_X;
+    }
+
+    @Override
+    public int getY() {
+        return this.RECT_Y;
+    }
+
+    public Color getColor() {
+        return this.RECT_COLOR;
+    }
+}
